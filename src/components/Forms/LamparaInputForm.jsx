6ed5e997729a5419bd94222b0baad030e6de6b46
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2024-01-31 15:36:42 +0800

    feat: implemented react hook form

--- src/components/Forms/LamparaInputForm.jsx
+++ src/components/Forms/LamparaInputForm.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const LamparaInputForm = ({
-	onChange,
 	type = 'text',
 	disabled,
 	placeholder,
 	label,
 	required = true,
+	name,
+	register,
+	errorMsg,
 }) => {
 	return (
 		<div className="flex flex-col justify-center py-2">
@@ -14,14 +16,13 @@ const LamparaInputForm = ({
 				{label} {required ? <span className="text-red-500">*</span> : ''}
 			</p>
 			<input
-				className="text-sm h-10 px-2 border rounded-md"
+				{...register(name)}
+				className="text-sm h-8 px-2 border rounded-md"
 				type={type}
-				name={label}
 				disabled={disabled}
-				required={required}
 				placeholder={placeholder}
-				onChange={onChange}
 			/>
+			<p className="text-red-500 text-xs">{errorMsg}</p>
 		</div>
 	);
 };
