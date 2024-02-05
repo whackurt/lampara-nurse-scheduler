f86b47e2b3e8317cb54f7d8ce069d7ad61bfa79a
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2024-02-05 13:21:22 +0800

    refactor: modified modal width

--- src/components/Modal/CustomModal.jsx
+++ src/components/Modal/CustomModal.jsx
@@ -4,7 +4,7 @@ const CustomModal = ({ children, showModal, toggleModal, title }) => {
 	return (
 		showModal && (
 			<div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
-				<div className="absolute bg-white w-3/4 md:w-1/2 lg:w-1/4 p-6 rounded shadow-lg">
+				<div className="absolute bg-white w-full max-w-[450px] p-8 rounded shadow-lg">
 					<span
 						onClick={toggleModal}
 						className="cursor-pointer text-2xl absolute top-0 right-0 p-4"
@@ -14,7 +14,7 @@ const CustomModal = ({ children, showModal, toggleModal, title }) => {
 					<h2 className="font-bold text-primary">{title}</h2>
 					<hr />
 					<div>
-						<div className="flex flex-col py-4 max-h-[400px] overflow-y-auto">
+						<div className="flex flex-col py-4 max-h-[500px] overflow-y-auto">
 							{children}
 						</div>
 					</div>
